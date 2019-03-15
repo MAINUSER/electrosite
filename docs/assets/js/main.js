a578226357a062d9cc186e430da0791189ff960c
MAINUSER <q2zx.xz2q@gmail.com>
2019-03-15 15:51:20 +0200

    span to img

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,7 @@
 var config = {
-	
+
 	"logo": "ELECTRIC WORK",
-	
+
     "menu": {
 		"": "головна",
         "#about": "ПРО НАС",
@@ -9,86 +9,86 @@ var config = {
 		"#expertise": "Професійний електромонтаж",
 		"#workstation": "НАШІ ПРОЕКТИ",
 		"#contact": "контакти",
-    },	
-	
+    },
+
     "title": "ELECTRIC WORK",
 	"title_text": "Електромонтажні роботи найвищої якості",
 	"contact_btn_text": "Наші контакти",
-		
+
 	"about": "ПРО КОМПАНІЮ",
 	"about_text": "uButia is a free bootstrap template created by freshdesignweb teams. The template includes the complete source files for download such as HTML5, CSS, CSS3 and JavaScript for easy customization.",
 	"about_span": "ELECTRIC WORK",
-	
+
     "services": "НАШІ ПОСЛУГИ",
     "service_list": [
 		{
 		"service_title": "РОЗВЕДЕННЯ (ЗАМІНА) ПРОВОДКИ",
 		"service_text": "Електромонтажні роботи найвищої якості",
-		"service_icon": "graphic-design",		
+		"service_icon": "assets/images/graphic-design.png",
 		},
 		{
 		"service_title": "ВСТАНОВЛЕННЯ СВІТИЛЬНИКІВ, ВИМИКАЧІВ, РОЗЕТОК",
 		"service_text": "Електромонтажні роботи найвищої якості",
-		"service_icon": "webdevelopment",		
+		"service_icon": "assets/images/printing.png",
 		},
 		{
 		"service_title": "МОНТАЖ ЕЛЕКТРОЩИТКА",
 		"service_text": "Електромонтажні роботи найвищої якості",
-		"service_icon": "printing",		
-		}		
-    ],	
-		
+		"service_icon": "assets/images/webdevelopment.png",		
+		}
+    ],
+
     "expertise": "Професійний електромонтаж",
 	"expertise_list": [
         "Довірте електромонтажні роботи нам, не економте на своїй безпеці! Ми гарантуємо, що електрика в вашій квартирі буде працювати чітко та безперебійно, відповідно до існуючих норм експлуатації та правил. Допоможемо придбати електроматеріали за дилерськими цінами.",
         "Допоможемо придбати електроматеріали за дилерськими цінами."
     ],
-	
+
 	"quote": "Єдиний спосіб робити щось добре – це по-справжньому любити те, чим ви займаєтеся.",
 	"author": "Стів Джобс",
-	
+
 	"workstation": "НАШІ ПРОЕКТИ",
     "workstation_list":  [
 		{
 		"workstation_title": "Creative Business Cup",
 		"workstation_text": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consetetur,adipisivelit, sed quia non numquam eius modi tempora incidun.",
-		"workstation_icon": "assets/images/4.jpg",		
+		"workstation_icon": "assets/images/4.jpg",
 		},
 		{
 		"workstation_title": "Creative Business Cup",
 		"workstation_text": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consetetur,adipisivelit, sed quia non numquam eius modi tempora incidun.",
-		"workstation_icon": "assets/images/5.jpg",		
+		"workstation_icon": "assets/images/5.jpg",
 		},
 		{
 		"workstation_title": "Creative Business Cup",
 		"workstation_text": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consetetur,adipisivelit, sed quia non numquam eius modi tempora incidun.",
-		"workstation_icon": "assets/images/6.jpg",		
+		"workstation_icon": "assets/images/6.jpg",
 		},
 		{
 		"workstation_title": "Creative Business Cup",
 		"workstation_text": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consetetur,adipisivelit, sed quia non numquam eius modi tempora incidun.",
-		"workstation_icon": "assets/images/8.jpg",		
+		"workstation_icon": "assets/images/8.jpg",
 		},
 		{
 		"workstation_title": "Creative Business Cup",
 		"workstation_text": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consetetur,adipisivelit, sed quia non numquam eius modi tempora incidun.",
-		"workstation_icon": "assets/images/9.jpg",		
+		"workstation_icon": "assets/images/9.jpg",
 		},
 		{
 		"workstation_title": "Creative Business Cup",
 		"workstation_text": "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consetetur,adipisivelit, sed quia non numquam eius modi tempora incidun.",
-		"workstation_icon": "assets/images/6.jpg",		
-		}		
+		"workstation_icon": "assets/images/6.jpg",
+		}
     ],
 
 	"subscribe": "Залиште Ваш номер телефону і наш менеджер зв'яжеться з Вами",
-	"placeholder": "Ваш номер телефону...",	
+	"placeholder": "Ваш номер телефону...",
 	"submit_btn": "Передзвоніть Мені",
-	"form": "https://getsimpleform.com/messages?form_api_token=df278bf7c3079e4df07bddd825c188a3",		
+	"form": "https://getsimpleform.com/messages?form_api_token=df278bf7c3079e4df07bddd825c188a3",
 
 	"contact_title": "Наші контакти",
 	"address": {"Адреса":"Level 5, 25 pitt st, US"},
-	"phone": {"Телефон":"+555 211 3747"},	
+	"phone": {"Телефон":"+555 211 3747"},
 	"mail": {"Email":"hey@freshdesignweb.com"},
-	
+
 };
